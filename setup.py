ea16d3d4fd8a35d01efad21861f9219c6f6e61f5
Hiranya Jayathilaka <hkj@google.com>
2017-03-23 14:14:33 -0700

    Updated licensing information (removing OSS license info temporarily)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     author_email='firebase-talk@googlegroups.com',
 
     # Choose your license
-    license='Apache Software License 2.0',
+    license='https://firebase.google.com/terms/',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -41,9 +41,6 @@ setup(
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.

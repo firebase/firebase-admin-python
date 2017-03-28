837f0a792374e33430ff3a004fde58641b891ae0
Hiranya Jayathilaka <hkj@google.com>
2017-03-28 11:02:13 -0700

    Declared six dependency explicitly (also included transitively via oauth2client)

--- setup.py
+++ setup.py
@@ -54,7 +54,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['oauth2client'],
+    install_requires=[
+        'oauth2client>=4.0.0',
+        'six>=1.6.1'
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

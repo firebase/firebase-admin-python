29837bbeb263a562043f8f49fd0eec6579d7bd3c
jwngr <jacob@firebase.com>
2017-04-05 17:02:28 -0700

    Updated license to Apache 2.0

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Setup file for distribution artifacts."""
 from __future__ import print_function
 
@@ -31,7 +45,7 @@ setup(
     long_description=long_description,
     url='https://firebase.google.com/docs/admin/setup/',
     author='Firebase',
-    license='https://firebase.google.com/terms/',
+    license='Apache License 2.0',
     keywords='firebase cloud development',
     install_requires=install_requires,
     packages=find_packages(exclude=['tests']),

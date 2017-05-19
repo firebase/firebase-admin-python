5539e71da41043d20ee184528ed86d4eb2e1c629
Hiranya Jayathilaka <hiranya911@gmail.com>
2017-05-19 11:37:35 -0700

    More argument validation and other best practices for bash commands

--- lint.sh
+++ lint.sh
@@ -17,11 +17,6 @@
 function lintAllFiles () {
   echo "Running linter on module $1"
   pylint --disable=$2 $1
-  rc=$?
-  if [ $rc -ne 0 ]
-  then
-    exit $rc
-  fi
 }
 
 function lintChangedFiles () {
@@ -30,21 +25,30 @@ function lintChangedFiles () {
   do
     echo "Running linter on $f"
     pylint --disable=$2 $f
-    rc=$?
-    if [ $rc -ne 0 ]
-    then
-      exit $rc
-    fi
   done
 }
 
+set -o errexit
+set -o nounset
+
 SKIP_FOR_TESTS="redefined-outer-name,protected-access,missing-docstring"
 
-if [[ $1 = "all" ]]
+if [[ "$#" -eq 1 && "$1" = "all" ]]
 then
-  lintAllFiles firebase_admin
-  lintAllFiles tests $SKIP_FOR_TESTS
+  CHECK_ALL=true  
+elif [[ "$#" -eq  0 ]]
+then
+  CHECK_ALL=false
 else
-  lintChangedFiles firebase_admin
-  lintChangedFiles tests $SKIP_FOR_TESTS
+  echo "Usage: ./lint.sh [all]"
+  exit 1
+fi
+
+if [[ "$CHECK_ALL" = true ]]
+then
+  lintAllFiles "firebase_admin" ""
+  lintAllFiles "tests" "$SKIP_FOR_TESTS"
+else
+  lintChangedFiles "firebase_admin" ""
+  lintChangedFiles "tests" "$SKIP_FOR_TESTS"
 fi

9f30b17a521f45f1d10671ab23eca76aaee17b70
Hiranya Jayathilaka <hiranya911@gmail.com>
2017-05-26 16:42:21 -0700

    Adding license headers to new files; Using the same testutils from unit tests

--- integration/conftest.py
+++ integration/conftest.py
@@ -1,3 +1,17 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 
 import pytest
@@ -27,5 +41,3 @@ def default_app(request):
     cred = credentials.Certificate(cert_path)
     ops = {'dbURL' : 'https://{0}.firebaseio.com'.format(project_id)}
     return firebase_admin.initialize_app(cred, ops)
-
-

49bc54c415b75300be889b7a8fed8e3cec1c0090
Rodolpho Pivetta Sabino <rodolpho.pivetta@gmail.com>
2017-10-17 10:18:49 -0200

    #61 add support to shallow param

--- firebase_admin/db.py
+++ firebase_admin/db.py
@@ -125,11 +125,12 @@ class Reference(object):
         full_path = self._pathurl + '/' + path
         return Reference(client=self._client, path=full_path)
 
-    def get(self, etag=False):
+    def get(self, etag=False, shallow=False):
         """Returns the value, and optionally the ETag, at the current location of the database.
 
         Args:
           etag: A boolean indicating whether the Etag value should be returned or not (optional).
+          shallow: A boolean indicating whether the result will be shallow or not (optional).
 
         Returns:
           object: If etag is False returns the decoded JSON value of the current database location.
@@ -141,10 +142,12 @@ class Reference(object):
         """
         if etag:
             headers, data = self._client.headers_and_body(
-                'get', self._add_suffix(), headers={'X-Firebase-ETag' : 'true'})
+                'get', self._add_suffix(), headers={'X-Firebase-ETag' : 'true'},
+                params={'shallow': shallow})
             return data, headers.get('ETag')
         else:
-            return self._client.body('get', self._add_suffix())
+            return self._client.body('get', self._add_suffix(),
+                params={'shallow': shallow})
 
     def get_if_changed(self, etag):
         """Gets data in this location only if the specified ETag does not match.

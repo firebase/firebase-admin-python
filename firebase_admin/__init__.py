4704987260e559fd91494278ef7ed934f93bdf16
Avishalom Shalit <avishalom@gmail.com>
2018-01-11 14:57:45 -0500

    version bump 2.8.0

--- firebase_admin/__init__.py
+++ firebase_admin/__init__.py
@@ -25,7 +25,7 @@ from firebase_admin import credentials
 
 # Declaring module version as per https://www.python.org/dev/peps/pep-0396/#specification
 # Update this accordingly for each release.
-__version__ = '2.7.0'
+__version__ = '2.8.0'
 
 _apps = {}
 _apps_lock = threading.RLock()

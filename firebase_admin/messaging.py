bf783c313348f4c48f2b8c9bd5df52d617051989
Hiranya Jayathilaka <hiranya911@gmail.com>
2018-02-28 17:49:08 -0800

    Handling FCM canonical error codes (#131)

--- firebase_admin/messaging.py
+++ firebase_admin/messaging.py
@@ -716,11 +716,18 @@ class _MessagingService(object):
     INTERNAL_ERROR = 'internal-error'
     UNKNOWN_ERROR = 'unknown-error'
     FCM_ERROR_CODES = {
-        'APNS_AUTH_ERROR': 'authentication-error',
+        # FCM v1 canonical error codes
+        'NOT_FOUND': 'registration-token-not-registered',
+        'PERMISSION_DENIED': 'mismatched-credential',
+        'RESOURCE_EXHAUSTED': 'message-rate-exceeded',
+        'UNAUTHENTICATED': 'invalid-apns-credentials',
+
+        # FCM v1 new error codes
+        'APNS_AUTH_ERROR': 'invalid-apns-credentials',
         'INTERNAL': INTERNAL_ERROR,
         'INVALID_ARGUMENT': 'invalid-argument',
         'QUOTA_EXCEEDED': 'message-rate-exceeded',
-        'SENDER_ID_MISMATCH': 'authentication-error',
+        'SENDER_ID_MISMATCH': 'mismatched-credential',
         'UNAVAILABLE': 'server-unavailable',
         'UNREGISTERED': 'registration-token-not-registered',
     }

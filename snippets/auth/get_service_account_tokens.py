e27af914c92b5731f7c3ae43f1c9a6e35e1cb22c
Nicolas Couture <nicolas.couture@gmail.com>
2018-05-15 03:42:06 -0400

    adjusting snippets to lint happily on Python <3
     - added "from __future__ import print_function" in snippets using
       print to make pylint "print-function-aware" and lint Python 3.x
       print-syntax without errors when running under Python 2.x.

--- snippets/auth/get_service_account_tokens.py
+++ snippets/auth/get_service_account_tokens.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
+
 # [START get_service_account_tokens]
 from firebase_admin import credentials
 
--- snippets/database/index.py
+++ snippets/database/index.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db

8d8e4ebe2ad5458b61610b54d5f0808cc0ca1291
hiranya911 <hiranya911@gmail.com>
2020-03-30 14:06:41 -0700

    Added documentation to Client APIs

--- firebase_admin/auth.py
+++ firebase_admin/auth.py
@@ -240,6 +240,14 @@ def revoke_refresh_tokens(uid, app=None):
     existing sessions from getting minted, existing ID tokens may remain active until their
     natural expiration (one hour). To verify that ID tokens are revoked, use
     ``verify_id_token(idToken, check_revoked=True)``.
+
+    Args:
+        uid: A user ID string.
+        app: An App instance (optional).
+
+    Raises:
+        ValueError: If the user ID is None, empty or malformed.
+        FirebaseError: If an error occurs while revoking the refresh token.
     """
     client = _get_client(app)
     client.revoke_refresh_tokens(uid)
@@ -379,8 +387,9 @@ def update_user(uid, **kwargs): # pylint: disable=differing-param-doc
         disabled: A boolean indicating whether or not the user account is disabled (optional).
         custom_claims: A dictionary or a JSON string contining the custom claims to be set on the
             user account (optional). To remove all custom claims, pass ``auth.DELETE_ATTRIBUTE``.
-        valid_since: An integer signifying the seconds since the epoch. This field is set by
-            ``revoke_refresh_tokens`` and it is discouraged to set this field directly.
+        valid_since: An integer signifying the seconds since the epoch (optional). This field is
+            set by ``revoke_refresh_tokens`` and it is discouraged to set this field directly.
+        app: An App instance (optional).
 
     Returns:
         UserRecord: An updated UserRecord instance for the user.
@@ -556,11 +565,45 @@ class Client:
         return self._tenant_id
 
     def create_custom_token(self, uid, developer_claims=None):
+        """Builds and signs a Firebase custom auth token.
+
+        Args:
+            uid: ID of the user for whom the token is created.
+            developer_claims: A dictionary of claims to be included in the token
+                (optional).
+
+        Returns:
+            bytes: A token minted from the input parameters.
+
+        Raises:
+            ValueError: If input parameters are invalid.
+            TokenSignError: If an error occurs while signing the token using the remote IAM service.
+        """
         return self._token_generator.create_custom_token(
             uid, developer_claims, tenant_id=self.tenant_id)
 
     def verify_id_token(self, id_token, check_revoked=False):
-        """Verifies the signature and data for the provided ID token."""
+        """Verifies the signature and data for the provided JWT.
+
+        Accepts a signed token string, verifies that it is current, and issued
+        to this project, and that it was correctly signed by Google.
+
+        Args:
+            id_token: A string of the encoded JWT.
+            check_revoked: Boolean, If true, checks whether the token has been revoked (optional).
+
+        Returns:
+            dict: A dictionary of key-value pairs parsed from the decoded JWT.
+
+        Raises:
+            ValueError: If ``id_token`` is a not a string or is empty.
+            InvalidIdTokenError: If ``id_token`` is not a valid Firebase ID token.
+            ExpiredIdTokenError: If the specified ID token has expired.
+            RevokedIdTokenError: If ``check_revoked`` is ``True`` and the ID token has been
+                revoked.
+            CertificateFetchError: If an error occurs while fetching the public key certificates
+                required to verify the ID token.
+        """
         if not isinstance(check_revoked, bool):
             # guard against accidental wrong assignment.
             raise ValueError('Illegal check_revoked argument. Argument must be of type '
@@ -578,54 +621,282 @@ class Client:
         return verified_claims
 
     def revoke_refresh_tokens(self, uid):
+        """Revokes all refresh tokens for an existing user.
+
+        revoke_refresh_tokens updates the user's tokens_valid_after_timestamp to the current UTC
+        in seconds since the epoch. It is important that the server on which this is called has its
+        clock set correctly and synchronized.
+
+        While this revokes all sessions for a specified user and disables any new ID tokens for
+        existing sessions from getting minted, existing ID tokens may remain active until their
+        natural expiration (one hour). To verify that ID tokens are revoked, use
+        ``verify_id_token(idToken, check_revoked=True)``.
+
+        Args:
+            uid: A user ID string.
+
+        Raises:
+            ValueError: If the user ID is None, empty or malformed.
+            FirebaseError: If an error occurs while revoking the refresh token.
+        """
         self._user_manager.update_user(uid, valid_since=int(time.time()))
 
     def get_user(self, uid):
+        """Gets the user data corresponding to the specified user ID.
+
+        Args:
+            uid: A user ID string.
+
+        Returns:
+            UserRecord: A UserRecord instance.
+
+        Raises:
+            ValueError: If the user ID is None, empty or malformed.
+            UserNotFoundError: If the specified user ID does not exist.
+            FirebaseError: If an error occurs while retrieving the user.
+        """
         response = self._user_manager.get_user(uid=uid)
         return UserRecord(response)
 
     def get_user_by_email(self, email):
+        """Gets the user data corresponding to the specified user email.
+
+        Args:
+            email: A user email address string.
+
+        Returns:
+            UserRecord: A UserRecord instance.
+
+        Raises:
+            ValueError: If the email is None, empty or malformed.
+            UserNotFoundError: If no user exists by the specified email address.
+            FirebaseError: If an error occurs while retrieving the user.
+        """
         response = self._user_manager.get_user(email=email)
         return UserRecord(response)
 
     def get_user_by_phone_number(self, phone_number):
+        """Gets the user data corresponding to the specified phone number.
+
+        Args:
+            phone_number: A phone number string.
+
+        Returns:
+            UserRecord: A UserRecord instance.
+
+        Raises:
+            ValueError: If the phone number is None, empty or malformed.
+            UserNotFoundError: If no user exists by the specified phone number.
+            FirebaseError: If an error occurs while retrieving the user.
+        """
         response = self._user_manager.get_user(phone_number=phone_number)
         return UserRecord(response)
 
     def list_users(self, page_token=None, max_results=_user_mgt.MAX_LIST_USERS_RESULTS):
+        """Retrieves a page of user accounts from a Firebase project.
+
+        The ``page_token`` argument governs the starting point of the page. The ``max_results``
+        argument governs the maximum number of user accounts that may be included in the returned
+        page. This function never returns None. If there are no user accounts in the Firebase
+        project, this returns an empty page.
+
+        Args:
+            page_token: A non-empty page token string, which indicates the starting point of the
+                page (optional). Defaults to ``None``, which will retrieve the first page of users.
+            max_results: A positive integer indicating the maximum number of users to include in
+                the returned page (optional). Defaults to 1000, which is also the maximum number
+                allowed.
+
+        Returns:
+            ListUsersPage: A ListUsersPage instance.
+
+        Raises:
+            ValueError: If max_results or page_token are invalid.
+            FirebaseError: If an error occurs while retrieving the user accounts.
+        """
         def download(page_token, max_results):
             return self._user_manager.list_users(page_token, max_results)
         return ListUsersPage(download, page_token, max_results)
 
-    def create_user(self, **kwargs):
+    def create_user(self, **kwargs): # pylint: disable=differing-param-doc
+        """Creates a new user account with the specified properties.
+
+        Args:
+            kwargs: A series of keyword arguments (optional).
+
+        Keyword Args:
+            uid: User ID to assign to the newly created user (optional).
+            display_name: The user's display name (optional).
+            email: The user's primary email (optional).
+            email_verified: A boolean indicating whether or not the user's primary email is
+                verified (optional).
+            phone_number: The user's primary phone number (optional).
+            photo_url: The user's photo URL (optional).
+            password: The user's raw, unhashed password. (optional).
+            disabled: A boolean indicating whether or not the user account is disabled (optional).
+
+        Returns:
+            UserRecord: A UserRecord instance for the newly created user.
+
+        Raises:
+            ValueError: If the specified user properties are invalid.
+            FirebaseError: If an error occurs while creating the user account.
+        """
         uid = self._user_manager.create_user(**kwargs)
         return self.get_user(uid=uid)
 
-    def update_user(self, uid, **kwargs):
+    def update_user(self, uid, **kwargs): # pylint: disable=differing-param-doc
+        """Updates an existing user account with the specified properties.
+
+        Args:
+            uid: A user ID string.
+            kwargs: A series of keyword arguments (optional).
+
+        Keyword Args:
+            display_name: The user's display name (optional). Can be removed by explicitly passing
+                ``auth.DELETE_ATTRIBUTE``.
+            email: The user's primary email (optional).
+            email_verified: A boolean indicating whether or not the user's primary email is
+                verified (optional).
+            phone_number: The user's primary phone number (optional). Can be removed by explicitly
+                passing ``auth.DELETE_ATTRIBUTE``.
+            photo_url: The user's photo URL (optional). Can be removed by explicitly passing
+                ``auth.DELETE_ATTRIBUTE``.
+            password: The user's raw, unhashed password. (optional).
+            disabled: A boolean indicating whether or not the user account is disabled (optional).
+            custom_claims: A dictionary or a JSON string contining the custom claims to be set on
+                the user account (optional). To remove all custom claims, pass
+                ``auth.DELETE_ATTRIBUTE``.
+            valid_since: An integer signifying the seconds since the epoch (optional). This field
+                is set by ``revoke_refresh_tokens`` and it is discouraged to set this field
+                directly.
+
+        Returns:
+            UserRecord: An updated UserRecord instance for the user.
+
+        Raises:
+            ValueError: If the specified user ID or properties are invalid.
+            FirebaseError: If an error occurs while updating the user account.
+        """
         self._user_manager.update_user(uid, **kwargs)
         return self.get_user(uid=uid)
 
     def set_custom_user_claims(self, uid, custom_claims):
+        """Sets additional claims on an existing user account.
+
+        Custom claims set via this function can be used to define user roles and privilege levels.
+        These claims propagate to all the devices where the user is already signed in (after token
+        expiration or when token refresh is forced), and next time the user signs in. The claims
+        can be accessed via the user's ID token JWT. If a reserved OIDC claim is specified (sub,
+        iat, iss, etc), an error is thrown. Claims payload must also not be larger then 1000
+        characters when serialized into a JSON string.
+
+        Args:
+            uid: A user ID string.
+            custom_claims: A dictionary or a JSON string of custom claims. Pass None to unset any
+                claims set previously.
+
+        Raises:
+            ValueError: If the specified user ID or the custom claims are invalid.
+            FirebaseError: If an error occurs while updating the user account.
+        """
         if custom_claims is None:
             custom_claims = DELETE_ATTRIBUTE
         self._user_manager.update_user(uid, custom_claims=custom_claims)
 
     def delete_user(self, uid):
+        """Deletes the user identified by the specified user ID.
+
+        Args:
+            uid: A user ID string.
+
+        Raises:
+            ValueError: If the user ID is None, empty or malformed.
+            FirebaseError: If an error occurs while deleting the user account.
+        """
         self._user_manager.delete_user(uid)
 
     def import_users(self, users, hash_alg=None):
+        """Imports the specified list of users into Firebase Auth.
+
+        At most 1000 users can be imported at a time. This operation is optimized for bulk imports
+        and will ignore checks on identifier uniqueness which could result in duplications. The
+        ``hash_alg`` parameter must be specified when importing users with passwords. Refer to the
+        ``UserImportHash`` class for supported hash algorithms.
+
+        Args:
+            users: A list of ``ImportUserRecord`` instances to import. Length of the list must not
+                exceed 1000.
+            hash_alg: A ``UserImportHash`` object (optional). Required when importing users with
+                passwords.
+
+        Returns:
+            UserImportResult: An object summarizing the result of the import operation.
+
+        Raises:
+            ValueError: If the provided arguments are invalid.
+            FirebaseError: If an error occurs while importing users.
+        """
         result = self._user_manager.import_users(users, hash_alg)
         return UserImportResult(result, len(users))
 
     def generate_password_reset_link(self, email, action_code_settings=None):
+        """Generates the out-of-band email action link for password reset flows for the specified
+        email address.
+
+        Args:
+            email: The email of the user whose password is to be reset.
+            action_code_settings: ``ActionCodeSettings`` instance (optional). Defines whether
+                the link is to be handled by a mobile app and the additional state information to
+                be passed in the deep link.
+
+        Returns:
+            link: The password reset link created by the API
+
+        Raises:
+            ValueError: If the provided arguments are invalid
+            FirebaseError: If an error occurs while generating the link
+        """
         return self._user_manager.generate_email_action_link(
             'PASSWORD_RESET', email, action_code_settings=action_code_settings)
 
     def generate_email_verification_link(self, email, action_code_settings=None):
+        """Generates the out-of-band email action link for email verification flows for the
+        specified email address.
+
+        Args:
+            email: The email of the user to be verified.
+            action_code_settings: ``ActionCodeSettings`` instance (optional). Defines whether
+                the link is to be handled by a mobile app and the additional state information to
+                be passed in the deep link.
+
+        Returns:
+            link: The email verification link created by the API
+
+        Raises:
+            ValueError: If the provided arguments are invalid
+            FirebaseError: If an error occurs while generating the link
+        """
         return self._user_manager.generate_email_action_link(
             'VERIFY_EMAIL', email, action_code_settings=action_code_settings)
 
     def generate_sign_in_with_email_link(self, email, action_code_settings):
+        """Generates the out-of-band email action link for email link sign-in flows, using the
+        action code settings provided.
+
+        Args:
+            email: The email of the user signing in.
+            action_code_settings: ``ActionCodeSettings`` instance. Defines whether
+                the link is to be handled by a mobile app and the additional state information to be
+                passed in the deep link.
+
+        Returns:
+            link: The email sign-in link created by the API
+
+        Raises:
+            ValueError: If the provided arguments are invalid
+            FirebaseError: If an error occurs while generating the link
+        """
         return self._user_manager.generate_email_action_link(
             'EMAIL_SIGNIN', email, action_code_settings=action_code_settings)
 

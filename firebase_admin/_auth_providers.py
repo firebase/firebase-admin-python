38927b65bd115dd21eea915388bbd1ab0e6c7dc9
hiranya911 <hiranya911@gmail.com>
2020-04-01 16:31:45 -0700

    Moved auth.Client; Updated docs

--- firebase_admin/_auth_providers.py
+++ firebase_admin/_auth_providers.py
@@ -179,6 +179,7 @@ def _validate_non_empty_string(value, label):
 
 
 def _validate_url(url, label):
+    """Validates that the given value is a wellformed URL string."""
     if not isinstance(url, str) or not url:
         raise ValueError(
             'Invalid photo URL: "{0}". {1} must be a non-empty '

2d6731e695c963c112c9820147d0db0271aa822f
Hardik Shah <hardikns@gmail.com>
2021-09-20 13:36:19 +0530

    [fix] tests

--- tests/test_user_mgt.py
+++ tests/test_user_mgt.py
@@ -663,14 +663,14 @@ class TestUpdateUser:
         request = json.loads(recorder[0].body.decode())
         assert request == {'localId': 'testuser', 'validSince': int(arg)}
 
-    @pytest.mark.parametrize('arg', [[], ['phone'], ['google.com', 'phone']])
+    @pytest.mark.parametrize('arg', [['phone'], ['google.com', 'phone']])
     def test_update_user_delete_provider(self, user_mgt_app, arg):
         user_mgt, recorder = _instrument_user_manager(user_mgt_app, 200, '{"localId":"testuser"}')
         user_mgt.update_user('testuser', delete_providers=arg)
         request = json.loads(recorder[0].body.decode())
         assert request['deleteProvider'] == arg
 
-    @pytest.mark.parametrize('arg', [[], ['phone'], ['google.com', 'phone']])
+    @pytest.mark.parametrize('arg', [['phone'], ['google.com', 'phone']])
     def test_update_user_delete_provider_and_phone(self, user_mgt_app, arg):
         user_mgt, recorder = _instrument_user_manager(user_mgt_app, 200, '{"localId":"testuser"}')
         user_mgt.update_user('testuser', delete_providers=arg, phone_number=auth.DELETE_ATTRIBUTE)

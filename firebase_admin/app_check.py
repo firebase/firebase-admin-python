3c4e191f3599af3630a2caa95f531363ec0d12c4
dwyfrequency <jackdwyer@google.com>
2022-09-07 15:52:35 -0400

    Fix some lint issues

--- firebase_admin/app_check.py
+++ firebase_admin/app_check.py
@@ -16,16 +16,12 @@
 # def _get_messaging_service(app):
 #     return _utils.get_app_service(app, _MESSAGING_ATTRIBUTE, _MessagingService)
 
-# Our goal in general is to take the design doc implentation and match it to the 
-# existing code in the SDK with tests
-# Timeline is to be done by week of the 19th 
-
 """Firebase App Check module."""
 
 # ASK(lahiru) Do I need to add these imports to the requirements file?
+from typing import Any, Dict, List
 import jwt
 from jwt import PyJWKClient
-from typing import Any, Dict, List
 from firebase_admin import _utils
 
 _APP_CHECK_ATTRIBUTE = '_app_check'
@@ -39,10 +35,9 @@ def verify_token(token: str, app=None) -> Dict[str, Any]:
 
 class _AppCheckService:
     """Service class that implements Firebase App Check functionality."""
-    
+  
     _APP_CHECK_GCP_API_URL = "https://firebaseappcheck.googleapis.com"
-    _APP_CHECK_BETA_JWKS_RESOURCE = "/v1beta/jwks" 
-
+    _APP_CHECK_BETA_JWKS_RESOURCE = "/v1beta/jwks"
 
     def __init__(self, app):
         # the verification method should go in the service
@@ -68,17 +63,17 @@ class _AppCheckService:
         signing_key = jwks_client.get_signing_key_from_jwt(token)
 
         header = jwt.get_unverified_header(token)
-        self._has_valid_token_headers(header) 
-
+        self._has_valid_token_headers(header)
         
-        # I don't see any method or property to just get key from signing_key /*/lib/python3.10/site-packages/jwt/api_jwk.py
+        # I don't see any method or property to just get key
+        # from signing_key /*/lib/python3.10/site-packages/jwt/api_jwk.py
         payload = self._decode_and_verify(token, signing_key.key, "project_number")
 
         # The token's subject will be the app ID, you may optionally filter against
         # an allow list
         return payload.get('sub')
-    
-    def _has_valid_token_headers(header: Any) -> None:
+
+    def _has_valid_token_headers(self, header: Any) -> None:
         # Ensure the token's header has type JWT
         if header.get('typ') != 'JWT':
             raise ValueError("The token received is not a JWT")
@@ -86,11 +81,9 @@ class _AppCheckService:
         if header.get('alg') != 'RS256':
             raise ValueError("JWT's algorithm does not have valid token headers")
 
-    def _decode_token(token: str, signing_key: str, algorithms:List[str]=["RS256"]) -> Dict[str, Any]:
+    def _decode_token(self, token: str, signing_key: str, algorithms: List[str]) -> Dict[str, Any]:
         payload = {}
         try:
-            # Verify the signature on the App Check token
-            # Ensure the token is not expired
             payload = jwt.decode(
                 token,
                 signing_key,
@@ -99,25 +92,19 @@ class _AppCheckService:
         except:
             ValueError('Unable to decode the token')
         return payload
-    
-    # move inside service class 
-    def _decode_and_verify(self, token: str, signing_key: str, project_number: str):
-        payload = {} 
-        # Verify the signature on the App Check token
-        # Ensure the token is not expired
+
+    def _decode_and_verify(self, token: str, signing_key: str):
+        payload = {}
         payload = self._decode_token(
             token,
             signing_key,
             algorithms=["RS256"]
         )
+
+        # within the aud property, there will be an array of project id & number
         if len(payload.aud) <= 1:
             raise ValueError('Project ID and Project Number are required to access App Check.') 
         if self._APP_CHECK_GCP_API_URL not in payload.issuer:
             raise ValueError('Token does not contain the correct Issuer.')
 
-        # within the aud of the payload, there will be an array of project id & number
         return payload
-
-# we need to make some code around fetching the project id 
-
-# Instead of returning none, raise value errors exceptions see messaging

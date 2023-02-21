f19c0e97d6470864e84b04a8abb97a559a7dd31c
Pragati <pragatimodi@google.com>
2023-02-21 15:27:19 -0800

    Lint error fix

--- firebase_admin/multi_factor_config_mgt.py
+++ firebase_admin/multi_factor_config_mgt.py
@@ -168,7 +168,7 @@ def validate_mfa_config(mfa_config: MultiFactorConfig):
                         not 0 <= totp_provider_config.adjacent_intervals <= 10):
                     raise ValueError(
                         ('{0}.{1} must be a valid positive integer' +
-                        ' between 0 and 10 (both inclusive).').format(
+                         ' between 0 and 10 (both inclusive).').format(
                             MfaConstants.TOTP_PROVIDER_CONFIG,
                             MfaConstants.ADJACENT_INTERVALS))
                 totp_provider_config_payload[MfaConstants.ADJACENT_INTERVALS] = \

2252f17539067333bebff6f3ba1368e19fcc3ca4
Jonathan Edey <145066863+jonathanedey@users.noreply.github.com>
2024-01-16 14:33:45 -0500

    chore: Add python 3.11 and 3.12 to CI tests (#754)
    * Add python 3.11 and 3.12 to CI tests
    
    * fix: typo
    
    * added new versions to classifier  list
    
    * Trigger CI tests

--- setup.py
+++ setup.py
@@ -70,6 +70,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: Apache Software License',
     ],
 )
